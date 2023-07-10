040ab0f8be8e23648bc5b59796353993a09e7755
zooduck <zooduck@beniceoreatsomerice.com>
2023-07-10 03:03:25 +0100

    add scroll into view for animal details when selecting an animal

--- app/components/client/animal-details/animalDetails.component.tsx
+++ app/components/client/animal-details/animalDetails.component.tsx
@@ -1,42 +1,48 @@
 import { useSelector } from 'react-redux';
 import Image from 'next/image';
 import styles from './animalDetails.module.css';
+import { useRef } from 'react';
 
 export function AnimalDetails() {
-  const data = useSelector((state: { animals: AnimalData[][]; selectedAnimal: string; }) => {
+  const data: any = useSelector((state: { animals: AnimalData[][]; selectedAnimal: string; }) => {
     return state.animals.flat().find((animal) => {
       return animal.id === state.selectedAnimal;
-    }) || state.animals.flat()[0] || {};
+    }) || {};
   });
 
   const { name, description, image_url, image_width, image_height } = data;
   const imageSrc = `/${image_url}`;
+  const animalDetailsSection: any = useRef(null);
 
-    if (name && description) {
-      return (
-        <section className={styles['animal-details']}>
-        <h1 className={styles['animal-details__heading']}>{name}</h1>
+  setTimeout(() => {
+    animalDetailsSection.current?.scrollIntoView();
+  });
+
+  if (name && description) {
+    return (
+      <section className={styles['animal-details']} ref={animalDetailsSection}>
+      <h1 className={styles['animal-details__heading']}>{name}</h1>
+      <figure className={styles['animal-details__figure']}>
+        <Image
+          className={styles['animal-details__image']}
+          src={imageSrc}
+          alt={name}
+          priority={name === 'cow'}
+          width={image_width}
+          height={image_height}/>
+        <figcaption className={styles['animal-details__caption']}>{description}</figcaption>
+      </figure>
+    </section>
+    )
+  } else {
+    return (
+      <section className={styles['animal-details']}>
+        <h1 className={[styles['animal-details__heading'], styles['animal-details__heading--empty']].join(' ')}></h1>
         <figure className={styles['animal-details__figure']}>
-          <Image
-            className={styles['animal-details__image']}
-            src={imageSrc}
-            alt={name}
-            priority={name === 'cow'}
-            width={image_width}
-            height={image_height}/>
-          <figcaption className={styles['animal-details__caption']}>{description}</figcaption>
+          <section className={styles['animal-details__image-placeholder']}></section>
+          <figcaption className={[styles['animal-details__caption'], styles['animal-details__caption--empty']].join(' ')}>{description}</figcaption>
         </figure>
       </section>
-      )
-    } else {
-      return (
-        <section className={styles['animal-details']}>
-          <h1 className={[styles['animal-details__heading'], styles['animal-details__heading--empty']].join(' ')}></h1>
-          <figure className={styles['animal-details__figure']}>
-            <section className={styles['animal-details__image-placeholder']}></section>
-            <figcaption className={[styles['animal-details__caption'], styles['animal-details__caption--empty']].join(' ')}>{description}</figcaption>
-          </figure>
-        </section>
-      )
-    }
+    )
+  }
 }
